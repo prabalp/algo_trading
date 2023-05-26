112e44e02102f5416189692238cd7bea0b8abbaa
Ubuntu <ubuntu@ip-172-31-44-149.ap-south-1.compute.internal>
2023-05-26 05:45:25 +0000

    code updated

--- watcher/data_managment.py
+++ watcher/data_managment.py
@@ -1,12 +1,12 @@
 import sqlite3
 
 
-class data_managment():
+class data_managment:
     def __init__(self, db_name):
         conn = sqlite3.connect(db_name)
         self.cursor = conn.cursor()
         # creating the table
-        i_qur = '''
+        i_qur = """
             CREATE TABLE stocks
             (timestamp INTEGER PRIMARY KEY,
             symbol TEXT,
@@ -30,94 +30,89 @@ class data_managment():
             tot_buy_qty INTEGER,
             tot_sell_qty INTEGER
             )
-            '''
-        # self.cursor.execute(i_qur)    
+            """
+        # self.cursor.execute(i_qur)
         try:
             self.cursor.execute(i_qur)
-            print('new table created')
+            print("new table created")
         except:
-            print('table already present')
-        
+            print("table already present")
+
     def add_data(self, data):
-        # print('adding data '+data['symbol'])
-        # qur = '''INSERT INTO stocks (
-        #     timestamp,
-        #     symbol, 
-        #     fycode, 
-        #     fyFlag, 
-        #     pktLen, 
-        #     ltp, 
-        #     open_price, 
-        #     high_price, 
-        #     low_price, 
-        #     close_price, 
-        #     min_open_price, 
-        #     min_high_price, 
-        #     min_low_price, 
-        #     min_close_price, 
-        #     min_volume, 
-        #     last_traded_qty, 
-        #     last_traded_time, 
-        #     avg_trade_price, 
-        #     vol_traded_today, 
-        #     tot_buy_qty, 
-        #     tot_sell_qty)
-        #     VALUES (
-        #         {},
-        #         {}, 
-        #         {}, 
-        #         {}, 
-        #         {}, 
-        #         {}, 
-        #         {}, 
-        #         {}, 
-        #         {}, 
-        #         {}, 
-        #         {}, 
-        #         {}, 
-        #         {}, 
-        #         {}, 
-        #         {}, 
-        #         {}, 
-        #         {}, 
-        #         {}, 
-        #         {}, 
-        #         {}, 
-        #         {}
-        #     )'''.format(data['timestamp'],
-        #                 data['symbol'], 
-        #                 data['fycode'], 
-        #                 data['fyFlag'], 
-        #                 data['pktLen'], 
-        #                 data['ltp'], 
-        #                 data['open_price'], 
-        #                 data['high_price'], 
-        #                 data['low_price'], 
-        #                 data['close_price'], 
-        #                 data['min_open_price'], 
-        #                 data['min_high_price'], 
-        #                 data['min_low_price'], 
-        #                 data['min_close_price'], 
-        #                 data['min_volume'], 
-        #                 data['last_traded_qty'], 
-        #                 data['last_traded_time'], 
-        #                 data['avg_trade_price'], 
-        #                 data['vol_traded_today'], 
-        #                 data['tot_buy_qty'], 
-        #                 data['tot_sell_qty']) # write a querry to insert data
+        print("adding data " + data["symbol"])
+        qur = """INSERT INTO stocks (
+            timestamp,
+            symbol, 
+            fycode, 
+            fyFlag, 
+            pktLen, 
+            ltp, 
+            open_price, 
+            high_price, 
+            low_price, 
+            close_price, 
+            min_open_price, 
+            min_high_price, 
+            min_low_price, 
+            min_close_price, 
+            min_volume, 
+            last_traded_qty, 
+            last_traded_time, 
+            avg_trade_price, 
+            vol_traded_today, 
+            tot_buy_qty, 
+            tot_sell_qty)
+            VALUES (
+                {},
+                {}, 
+                {}, 
+                {}, 
+                {}, 
+                {}, 
+                {}, 
+                {}, 
+                {}, 
+                {}, 
+                {}, 
+                {}, 
+                {}, 
+                {}, 
+                {}, 
+                {}, 
+                {}, 
+                {}, 
+                {}, 
+                {}, 
+                {}
+            )""".format(
+            data["timestamp"],
+            data["symbol"],
+            data["fycode"],
+            data["fyFlag"],
+            data["pktLen"],
+            data["ltp"],
+            data["open_price"],
+            data["high_price"],
+            data["low_price"],
+            data["close_price"],
+            data["min_open_price"],
+            data["min_high_price"],
+            data["min_low_price"],
+            data["min_close_price"],
+            data["min_volume"],
+            data["last_traded_qty"],
+            data["last_traded_time"],
+            data["avg_trade_price"],
+            data["vol_traded_today"],
+            data["tot_buy_qty"],
+            data["tot_sell_qty"],
+        )  # write a querry to insert data
         # print(qur)
         self.cursor.execute(qur)
-        print('done')
-        
+        print("done")
+
     def view_data(self):
         # qur = """ SELECT * FROM stocks"""
         # self.cursor.execute(qur)
         res = self.cursor.fetchall()
         print(res)
-
-
-
-
-
-
-
--- watcher/get_stocks_data.py
+++ watcher/get_stocks_data.py
@@ -1,58 +1,58 @@
 import os
-from fyers_api.Websocket import ws 
+from fyers_api.Websocket import ws
 import data_managment
 import datetime
 
 curr_wd = os.getcwd()
 
-# fyers = fyersModel.FyersModel(client_id=client_id, token=access_token,log_path="/home/Desktop/apiV2") ----this part will be provided 
+# fyers = fyersModel.FyersModel(client_id=client_id, token=access_token,log_path="/home/Desktop/apiV2") ----this part will be provided
 
 
 # data managment codes
-db_name = 'nifty200_' + datetime.datetime.now().strftime('%x') + ".db"
-db = data_managment.data_managment('test.db')
+db_name = "nifty200_" + datetime.datetime.now().strftime("%x") + ".db"
+db = data_managment.data_managment("test.db")
 
 
-def get_hist_data(fyers, year, stock_ticker_symbol):
-  # year = "2021"
-  time_range = [["01-01", "03-31"], ["04-01","06-30"], ["07-01","09-30"], ["10-01","12-31"]]
-  # stock_ticker_symbol = "NSE:SBIN-EQ"
+# def get_hist_data(fyers, year, stock_ticker_symbol):
+#   # year = "2021"
+#   time_range = [["01-01", "03-31"], ["04-01","06-30"], ["07-01","09-30"], ["10-01","12-31"]]
+#   # stock_ticker_symbol = "NSE:SBIN-EQ"
 
-  i = 0 
-  for x in time_range:
-    date_from = year +"-"+x[0]
-    date_to = year +"-" + x[1]
-    ticker = {"symbol":stock_ticker_symbol,"resolution":"1","date_format":"1","range_from":date_from,"range_to":date_to,"cont_flag":"1"}
-    print(ticker) 
-    data = fyers.history(ticker)
-    if(data['s'] == "no_data"):
-      no_data.append(stock_ticker_symbol+ date_from + date_to)
-      continue
-    output = pd.DataFrame(data['candles'])
-    output.columns = ['epoch_time', 'open_value', 'highest_value', 'lowest_value', 'close_value', 'volume_traded']
-    dir = "data" + "/" +stock_ticker_symbol + "/" + year 
-    output_location =dir + "/" + date_from + "to" + date_to + ".csv"
-    if(i == 0):
-      os.makedirs(dir)
-    output.to_csv(output_location)
-    i = i+1
-  
+#   i = 0
+#   for x in time_range:
+#     date_from = year +"-"+x[0]
+#     date_to = year +"-" + x[1]
+#     ticker = {"symbol":stock_ticker_symbol,"resolution":"1","date_format":"1","range_from":date_from,"range_to":date_to,"cont_flag":"1"}
+#     print(ticker)
+#     data = fyers.history(ticker)
+#     if(data['s'] == "no_data"):
+#       no_data.append(stock_ticker_symbol+ date_from + date_to)
+#       continue
+#     output = pd.DataFrame(data['candles'])
+#     output.columns = ['epoch_time', 'open_value', 'highest_value', 'lowest_value', 'close_value', 'volume_traded']
+#     dir = "data" + "/" +stock_ticker_symbol + "/" + year
+#     output_location =dir + "/" + date_from + "to" + date_to + ".csv"
+#     if(i == 0):
+#       os.makedirs(dir)
+#     output.to_csv(output_location)
+#     i = i+1
+
+
 def custom_message(msg):
-  # change msg in perfect format
-  db.add_data(msg[0])
-  # print(msg[0]['symbol'])
-  # print ("Custome Message " + str(msg)) 
-
-def get_live_data(client_id, access_token, symbol, data_type = "symbolData"):
-  # ws.websocket_data = custom_message  
-  fyersSocket = ws.FyersSocket(access_token=client_id+':'+access_token,run_background=True,log_path=curr_wd+"/socket_logs/")
-  fyersSocket.websocket_data = custom_message
-  # fyersSocket = ws.FyersSocket(access_token="KPQVRS7JKZ-100",run_background=True,log_path=curr_wd)
-  fyersSocket.subscribe(symbol=symbol,data_type=data_type)
-  fyersSocket.keep_running()
+    # change msg in perfect format
+    # db.add_data(msg[0])
+    # print(msg[0]["symbol"])
+    print("Custome Message " + str(msg))
 
 
-
-
-
-    
+def get_live_data(client_id, access_token, symbol, data_type="symbolData"):
+    # ws.websocket_data = custom_message
+    fyersSocket = ws.FyersSocket(
+        access_token=client_id + ":" + access_token,
+        run_background=True,
+        log_path=curr_wd + "/socket_logs/",
+    )
+    fyersSocket.websocket_data = custom_message
+    # fyersSocket = ws.FyersSocket(access_token="KPQVRS7JKZ-100",run_background=True,log_path=curr_wd)
+    fyersSocket.subscribe(symbol=symbol, data_type=data_type)
+    fyersSocket.keep_running()
